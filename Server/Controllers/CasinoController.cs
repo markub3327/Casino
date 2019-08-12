166150df90e59b2d3be1a0baaeaaa3dfcd0b666a
Martin Horvath <marhor3327@gmail.com>
2019-08-12 19:05:33 +0200

    v2.0 - new design, more used plugins and so on

--- Server/Controllers/CasinoController.cs
+++ Server/Controllers/CasinoController.cs
@@ -7,140 +7,78 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Casino.Server.Controllers
 {
+    [ApiController]
     public class CasinoController : ControllerBase
     {
-        private readonly Models.ApiContext _context;
+        private readonly Models.ApiContext context;
 
-        public CasinoController(Models.ApiContext context)
+        public CasinoController(ILogger<CasinoController> logger, Models.ApiContext context)
         {
-            _context = context;
-
-            Console.WriteLine("Main Casino's page");
-            Console.WriteLine($"Pocet hracov na serveri: {_context.Players.Count()}");
+            this.context = context;
         }
 
-        // GET casino/
-        [HttpGet]
-        public async Task<IActionResult> Index()
+        // GET http://localhost:5000/casino/players/all
+        [HttpGet("[controller]/players/all")]
+        public async Task<IActionResult> GetPlayers()
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            using (var stream = assembly.GetManifestResourceStream("Casino.Server.index.html"))
+            // Vytvor pole hracov na serveri
+            var players = await context.Players.Select(p => new
             {
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    return new ContentResult
-                    {
-                        ContentType = "text/html",
-                        StatusCode = StatusCodes.Status200OK,
-                        Content = await reader.ReadToEndAsync()
-                    };
-                }
-            }
+                p.Nickname,
+                p.GameId
+            }).ToListAsync();
+
+            // Vytvor vystup JSON zo zoznamu hracov pri stole
+            return Ok(players);
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Styles()
+        // GET http://localhost:5000/casino/players/player
+        [HttpGet("[controller]/players/player")]
+        public async Task<IActionResult> GetPlayer([FromBody]Items.Player player)
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            using (var stream = assembly.GetManifestResourceStream("Casino.Server.styles.css"))
+            if (player != null)
             {
-                using (StreamReader reader = new StreamReader(stream))
+                var playerDb = await context.Players.FindAsync(player.Nickname);
+
+                // Autorizacia hraca pomocou generovaneho tokenu
+                if (playerDb != null && playerDb.Token == player.Token)
                 {
-                    return new ContentResult
-                    {
-                        ContentType = "text/css",
-                        StatusCode = StatusCodes.Status200OK,
-                        Content = await reader.ReadToEndAsync()
-                    };
+                    return Ok(playerDb);
                 }
-            }
-        }
 
-        // GET casino/players
-        // GET casino/players?token=XXX
-        [HttpGet("{controller}/players"), Produces("application/json")]
-        public async Task<IActionResult> GetPlayers([FromQuery(Name = "token")]string token)
-        {
-            Console.WriteLine($"token = {token}");
-
-            if (!string.IsNullOrEmpty(token))
-            {
-                // Nacitaj tabulku z Db, kde plati zhodnost tokenu!
-                var players = await _context.Players.Where(p =>
-                    (p.Token == token)).Include(p => p.Cards).ToListAsync();
-
-                // Ak existuje hrac (s tokenom)
-                if (players.Count > 0)
-                {
-                    return Ok(players[0]);
-                }
                 return NotFound();
             }
-            else
-            {
-                // Vytvor pole hracov na serveri
-                var players = await _context.Players.ToListAsync();
-
-                // Vytvor vystup JSON zo zoznamu hracov pri stole
-                return Ok(players.Select(p => new
-                {
-                    name = p.Name,
-                    gameid = p.GameId
-                }));
-            }
-        }
-
-        // POST casino/players
-        [HttpPost("{controller}/players"), Produces("application/json")]
-        public async Task<IActionResult> PostPlayer([FromBody]Items.Player player)
-        {
-            // Nesmie byt objekt hraca prazdny
-            if (player != null)
-            {
-                // Over ci uz hrac s rovnakym menom neexistuje
-                if (!await _context.Players.ContainsAsync(player))
-                {
-                    // Vygeneruj bezpecnostny token pre komunikaciu
-                    TokenGenerator.Generate(player);
-
-                    // Pridaj hraca do Db
-                    await _context.Players.AddAsync(player);
-                    await _context.SaveChangesAsync();
-
-                    return CreatedAtAction(nameof(GetPlayers), new { token = player.Token }, player);
-                }
-            }
 
             return BadRequest();
         }
 
-        // PUT casino/players
-        /*[HttpPut("{controller}/players"), Produces("application/json")]
-        public async Task<IActionResult> PutPlayer([FromQuery]string token, [FromBody]Items.Player player)
+        // POST http://localhost:5000/casino/players/player
+        [HttpPost("[controller]/players/create")]
+        public async Task<IActionResult> CreatePlayer([FromBody]Items.Player player)
         {
-            // Nesmie byt objekt hraca prazdny
-            if (player != null)
+            if (!await context.Players.ContainsAsync(player))
             {
-                // Nacitaj tabulku z Db, kde plati zhodnost tokenu!
-                var players = await _context.Players.Where(p => (p.Token == token)).ToListAsync();
+                // Novy hrac nehra este ziadnu hru
+                player.GameId = "Nothing";
 
-                // Ak existuje v Db hrac (s tokenom)
-                if (players.Count > 0)
-                {
-                    // Aktualizuj vyber hracovej hry
-                    players[0].GameId = player.GameId;
+                // Novy hraci dostanu 10.000$
+                if (player.Wallet <= 0)
+                    player.Wallet = 10000;
 
-                    // Uloz zmeny profilu hraca do Db
-                    await _context.SaveChangesAsync();
+                // Prida zaznam do Db
+                context.Players.Add(player);
+                await context.SaveChangesAsync();
 
-                    return NoContent();
-                }
+                // Vytvoreny uspesne, vrati profil
+                return CreatedAtAction(nameof(GetPlayer), player);
             }
 
+            // Zla poziadavka
             return BadRequest();
-        }*/
+        }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Sockets;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -15,15 +13,6 @@ namespace Casino.Server
     {
         public static void Main(string[] args)
         {
-            Run(args);
-        }
-
-        public static void Run(string[] args)
-        {
-            // Uvodna sprava
-            Head();
-
-            // Novy server
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -33,21 +22,5 @@ namespace Casino.Server
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-
-        private static void Head()
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Welcome to AI Casino Server");
-            Console.ResetColor();
-            Console.WriteLine("Martin Horvath\tCopyright 2019");
-            Console.WriteLine("University of Ss. Cyril and Methodius in Trnava");
-            Console.WriteLine("===============================================");
-            Console.WriteLine("Connect to https://{0}:5001/casino\n", GetIP());
-        }
-
-        private static string GetIP()
-        {
-            return /*Dns.GetHostAddresses(*/Dns.GetHostName()/*).ToString()*/;
-        }
     }
 }
